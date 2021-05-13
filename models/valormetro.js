22f8393a097423322612d941d8c0b737ac4b445f
Francisco G <gsfdevpool@protonmail.com>
2021-05-13 11:17:59 -0300

    Identação do arquivo

--- models/valormetro.js
+++ models/valormetro.js
@@ -1,18 +1,17 @@
 const conexao = require('../infraestrutura/conexao')
-     
-
-        class Cep  {
-            busca(valor) {
-                const sql = 'SELECT valor FROM valorCep WHERE cep=?'
-                conexao.query(sql, valor, (erro, resultados) => {
-                    if(erro) {
-                        console.log(erro)
-                    } else {
-                        console.log(resultados)
-                    }
-                })
+class Cep  {
+    busca(valor) {
+        const sql = 'SELECT valor FROM valorCep WHERE cep=?'
+        const resultado = conexao.query(sql, valor, (erro, resultados) => {
+            if(erro) {
+                console.log(erro)
+            } else {
+                console.log(resultados)
             }
-        }
+        })
+        return resultado
+    }
+}
         
         
         module.exports = new Cep
