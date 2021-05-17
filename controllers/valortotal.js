f88aa2ed921fcceb89aef8694aa6eda79929d89b
Francisco G <gsfdevpool@protonmail.com>
2021-05-16 22:43:57 -0300

    Add chamacep.js - Criado função de get em outra API com axios e retornando o valor total do terreno

--- controllers/valortotal.js
+++ controllers/valortotal.js
@@ -8,14 +8,11 @@ module.exports = app => {
           if(quantidademetros >= 10 && quantidademetros <= 10000){
               console.log('Está Certo!') 
               const resultado = await pegaCep(cepimovel);
-              res.json(resultado);
-              /*
-               Lógica aqui - Mágica
-               */
-      } else {
+              res.json(resultado.valor * quantidademetros);
+            } else {
                 res.json('CEP é obrigatório para o operação')
-          }     
-      } else{
+                }     
+        } else{
               res.send('Para o cáculo do imóvel deve ter no mínimo 10 metros e no máximo 100000 metros!')
       } 
   })    
